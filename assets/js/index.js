aae5c4d20c5f63b533e55bf1d4301f28edc5dc8b
BlackZero172 <Supermeega@outlook.com>
2021-12-02 22:04:43 +0200

    Added Landing Page,Reset Button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 const worldContainer = document.querySelector(".world-container");
+const landingPage = document.querySelector(".landing-page");
 const blocks = {
 	dirt: "dirt",
 	sky: "sky",
@@ -6,10 +7,13 @@ const blocks = {
 };
 let selectedTool = "";
 const populateWorld = () => {
+	worldContainer.innerHTML = "";
+	landingPage.style.display = "none";
 	for (let i = 1; i <= 21; i++) {
 		let tileRow = document.createElement("tr");
 		for (let j = 1; j <= 21; j++) {
 			let tile = document.createElement("td");
+
 			if (i < 15) {
 				tile.classList.add(blocks.sky);
 			} else if (i == 15) {
@@ -23,20 +27,27 @@ const populateWorld = () => {
 					if (selectedTool === "shovel") e.target.classList.replace(blocks.dirt, blocks.sky);
 				});
 			}
-
+			tile.id = `x:${j} y:${i}`;
 			tileRow.append(tile);
 		}
 		worldContainer.append(tileRow);
 	}
 };
-populateWorld();
-const toolButtons = document.querySelectorAll("button");
+const changeTile = (tilePosition, tileType) => {
+	document.querySelector(`#${tilePosition}`).classList.replace("sky", tileType);
+};
+// populateWorld();
+const toolButtons = document.querySelectorAll(".toolbar .tool");
 toolButtons.forEach((tool) => {
 	tool.addEventListener("click", (e) => {
 		toolButtons.forEach((tool) => {
-			tool.classList.remove("selected");
+			tool.id = "";
 		});
-		tool.classList.add("selected");
+		tool.id = "selected";
 		selectedTool = tool.getAttribute("data-tool");
 	});
 });
+const startButton = document.querySelector(".start-btn");
+startButton.addEventListener("click", populateWorld);
+const resetButton = document.querySelector(".reset-btn");
+resetButton.addEventListener("click", populateWorld);
